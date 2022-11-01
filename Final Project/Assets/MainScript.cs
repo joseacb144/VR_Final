63e97423d0bf725f9d295e594b9c0724b8a1bb07
Jose A. Chavez <joseacb144@gmail.com>
2022-11-01 00:24:11 -0500

    Score count added when clicking on sphere

--- Final Project/Assets/MainScript.cs
+++ Final Project/Assets/MainScript.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //for text
 
 public class MainScript : MonoBehaviour
 {
+
+    // GAME OBJECTS
+    public GameObject myRightHand; // SHOOTING
+    public Text scoreTextField;
+    public int score_count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,26 @@ public class MainScript : MonoBehaviour
     void Update()
     {
         
+
+        // SHOOTING
+        if(OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            Ray myRay = new Ray(myRightHand.transform.position, myRightHand.transform.forward);
+            RaycastHit myHits;
+
+            if(Physics.Raycast(myRay, out myHits, 1000f))
+            {
+                //myHits.transform.gameObject is the GameObject of what it hits
+
+                //Destroy(myHits.transform.gameObject);
+                GameObject whatIhit = myHits.transform.gameObject;
+
+                if(whatIhit.name == "Sphere")
+                {
+                    score_count += 1;
+                    scoreTextField.text = "Score: " + score_count;
+                }
+            }
+        }
     }
 }
